3ba5fcc7a681fea20989b60dcba2d6863f1a4002
Carolina Rojas Ríos <39276286+caobaknowme@users.noreply.github.com>
2023-12-21 17:26:32 -0600

    Funcionalidad Contactenos

--- Js/index.js
+++ Js/index.js
@@ -128,6 +128,20 @@ async function getCurrentUserName() {
     }
 }
 
+function sendToWhatsapp(){
+    let number="+50685019391";
+    let name = document.getElementById('name').value;
+	let email = document.getElementById('email').value;
+	let message = document.getElementById('message').value;
+
+	var url = "https://wa.me/" + number + "?text="
+	+ "Name : " +name+ "%0a"
+	+ "Email : " +email+ "%0a"
+	+ "Message : " +message+ "%0a%0a";
+
+	window.open(url, '_blank').focus();
+}
+
 $(document).ready(()=>{
 
     formularioCrearIncidencia = document.getElementById("frmCrearIncidencia");
